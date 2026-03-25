a9da500eec98716eca0a4a7f1937c6b9619bb41d
azadprimemedico <azadprime4@gmail.com>
2026-03-26 00:23:59 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,30 @@
+import { db } from "./firebase.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
+async function loadProducts(){
+ let snap=await getDocs(collection(db,"products"));
+ let html="";
+ snap.forEach(d=>{
+ let p=d.data();
+ html+=`${p.name} ₹${p.price}
+ <button onclick="del('${d.id}')">Delete</button><br>`;
+ });
+ list.innerHTML=html;
+}
+
+window.addProduct=async()=>{
+ await addDoc(collection(db,"products"),{
+  name:pname.value,
+  price:Number(price.value),
+  category:category.value,
+  image:image.value
+ });
+ loadProducts();
+}
+
+window.del=async(id)=>{
+ await deleteDoc(doc(db,"products",id));
+ loadProducts();
+}
+
+loadProducts();
