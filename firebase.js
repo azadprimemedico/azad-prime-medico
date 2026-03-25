e7b0783600301c9fffb1ee3fb5936407fcc2e468
azadprimemedico <azadprime4@gmail.com>
2026-03-26 00:21:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "azad-prime-medico.firebaseapp.com",
+  projectId: "azad-prime-medico"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
