9e3ea0cb81922a22140171880960c9979ad785d7
azadprimemedico <azadprime4@gmail.com>
2026-03-26 20:37:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCqpzQlN_qu53CKbPa_XFDnoyN6SgxhKRw",
   authDomain: "azad-prime-medico.firebaseapp.com",
-  projectId: "azad-prime-medico"
+  projectId: "azad-prime-medico",
+  storageBucket: "azad-prime-medico.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456:web:123456"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
