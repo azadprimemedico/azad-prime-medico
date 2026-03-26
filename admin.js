4875e22b29458dde66122a7f3a2429e25a683451
azadprimemedico <azadprime4@gmail.com>
2026-03-27 00:28:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,54 +1,15 @@
-import { db, storage } from "./firebase.js";
-import { collection, getDocs, addDoc, updateDoc, doc } 
+import { db } from "./firebase.js";
+import { collection, addDoc } 
 from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-import { ref, uploadBytes, getDownloadURL } 
-from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
-
-async function uploadImage(file){
- const storageRef = ref(storage,'products/'+file.name);
- await uploadBytes(storageRef,file);
- return await getDownloadURL(storageRef);
-}
-
-window.addProduct=async()=>{
- let name=pname.value;
- let price=pprice.value;
- let category=pcategory.value;
- let file=pimage.files[0];
-
- let image=await uploadImage(file);
-
+window.addProduct = async ()=>{
  await addDoc(collection(db,"products"),{
-  name,price:Number(price),category,image,stock:10
+  name:name.value,
+  price:Number(price.value),
+  category:category.value,
+  image:image.value,
+  stock:10
  });
 
  alert("Product Added");
 }
-
-async function loadOrders(){
- const snap = await getDocs(collection(db,"orders"));
- let html="";
- snap.forEach(d=>{
-  let o=d.data();
-  html+=`
-  <div>
-  ${o.name} | ₹${o.total}
-  <select onchange="updateStatus('${d.id}',this.value)">
-  <option>${o.status}</option>
-  <option>Pending</option>
-  <option>Packed</option>
-  <option>Shipped</option>
-  <option>Delivered</option>
-  </select>
-  </div><hr>`;
- });
- document.getElementById("orders").innerHTML=html;
-}
-
-window.updateStatus=async(id,status)=>{
- await updateDoc(doc(db,"orders",id),{status});
- alert("Updated");
-}
-
-loadOrders();
