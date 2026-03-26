14101340c010243138b7b501a762cd8c6cbb4b35
azadprimemedico <azadprime4@gmail.com>
2026-03-27 01:20:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,5 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqpzQlN_qu53CKbPa_XFDnoyN6SgxhKRw",
-  authDomain: "azad-prime-medico.firebaseapp.com
+  apiKey: "AIzaSyCqpzQLN_qu53CKbPa_XFDnoyN6SgxhKRw",
+  authDomain: "azad-prime-medico.firebaseapp.com",
+  projectId: "azad-prime-medico",
+  storageBucket: "azad-prime-medico.firebasestorage.app",
+  messagingSenderId: "111274201862",
+  appId: "1:111274201862:web:a052eff79eca9f7d1ac487"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
